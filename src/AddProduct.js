c8dccf300606e5ae9daf3b6e4d1a3af9cddbe5d7
anil sidhu <webanilsidhu@gmail.com>
2021-01-19 21:40:14 +0530

    code for add product done

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,40 +1,47 @@
 import Header from './Header'
-function AddProduct()
-{
-   async function onFileUpload (e) { 
-     
-        // Create an object of formData 
-        let itemN=e.target.files[0]
-        const formData = new FormData(); 
-       
-        // Update the formData object 
-        formData.append( 
-          "file", 
-          itemN 
-        ); 
-       
-        // Details of the uploaded file 
-        // console.log(this.state.selectedFile); 
-       
-        // Request made to the backend api 
-        // Send formData object 
-        let result = await fetch("http://localhost:8000/api/temp1", {
-            method: 'POST',
-            body: formData
-        }); 
-      }; 
-    return (
-        <div>
-            <Header />
-            <h1>Add Product</h1>
-            <div> 
-                <input type="file" onChange={onFileUpload} /> 
-                <button onClick={onFileUpload}> 
-                  Upload! 
-                </button> 
-            </div> 
-        </div>
-    )
+import { useState } from 'react'
+function AddProduct() {
+  const [name, setName] = useState("");
+  const [file, setFile] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  async function addProduct() {
+    console.warn(name, file, price, description)
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('price', price);
+    formData.append('name', name);
+    formData.append('description', description);
+    let result = await fetch("http://localhost:8000/api/addproduct", {
+      method: 'POST',
+      body: formData
+    });
+    alert("Data has been saved")
+
+
+  }
+  return (
+    <div>
+      <Header />
+      <div className="col-sm-6 offset-sm-3">
+        <br />
+        <input type="text" className="form-control"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="name" /> <br />
+        <input type="file" className="form-control"
+          onChange={(e) => setFile(e.target.files[0])}
+
+          placeholder="file" /> <br />
+        <input type="text" className="form-control"
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="price" /> <br />
+        <input type="text" className="form-control"
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="description" /> <br />
+        <button onClick={addProduct} className="btn btn-primary">Add Product</button>
+      </div>
+    </div>
+  )
 }
 
 export default AddProduct
