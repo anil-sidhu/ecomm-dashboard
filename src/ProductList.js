00eef27ff4e2977ba065ea109d28085b86c2222f
anil sidhu <webanilsidhu@gmail.com>
2021-02-04 19:56:00 +0530

    add delete product module

--- src/ProductList.js
+++ src/ProductList.js
@@ -4,12 +4,25 @@ import {Table} from 'react-bootstrap'
 function ProductList()
 {
     const [data,setData]=useState([]);
-    useEffect( async ()=>{
+    useEffect( ()=>{
+        getData();
+    },[])
+    async function deleteOperation(id)
+    {
+       let result=await  fetch("http://localhost:8000/api/delete/"+id,{
+            method:'DELETE'
+        });
+        result=await result.json();
+        console.warn(result)
+        getData();
+
+    }
+    async function getData()
+    {
         let result = await fetch("http://localhost:8000/api/list");
         result =await  result.json();
         setData(result)
-    },[])
-    console.warn("result",data)
+    }
     return (
         <div>
             <Header />
@@ -22,6 +35,8 @@ function ProductList()
                     <td>Price</td>
                     <td>Description</td>
                     <td>Image</td>
+                    <td>Operations</td>
+
                 </tr>
                 {
                     data.map((item)=>
@@ -31,6 +46,7 @@ function ProductList()
                         <td>{item.price}</td>
                         <td>{item.description}</td>
                         <td><img style={{width:100}}  src={"http://localhost:8000/"+item.file_path}/></td>
+                        <td><span onClick={()=>deleteOperation(item.id)} className="delete">Delete</span></td>
                     </tr>
                     )
                 }
